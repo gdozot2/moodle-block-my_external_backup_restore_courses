5b81b9688e1b6dba66a736fe5213f170c059412a
Grégory Dozot <gregory.dozot@heig-vd.ch>
2023-10-03 14:52:01 +0200

    Update current task to reflet real action

--- version.php
+++ version.php
@@ -26,8 +26,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2023060601;
-$plugin->release = '3.0.5';
+$plugin->version = 2023100208;
+$plugin->release = '4.0.0';
 $plugin->maturity   = MATURITY_STABLE;
 $plugin->requires = 2022041904;
 // Full name of the plugin (used for diagnostics).
